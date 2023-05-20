3e75f339dd90fae1eb146836acfa816d25799870
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-21 04:55:14 +0800

    feat(db): implement session creation wrapper

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,4 @@
+use model::MacAddress;
 use tokio_postgres::Client;
 
 pub struct Database {
@@ -7,5 +8,12 @@ pub struct Database {
 impl From<Client> for Database {
     fn from(db: Client) -> Self {
         Self { db }
+    }
+}
+
+impl Database {
+    pub async fn create_session(&self, mac: MacAddress) -> uuid::Uuid {
+        let row = self.db.query_one("INSERT INTO session (mac) VALUES ($1) RETURNING id", &[&mac]).await.unwrap();
+        row.get(0)
     }
 }
