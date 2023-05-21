da6d675fabe2df5144b89e54cd3036eea1953c23
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-21 15:00:40 +0800

    test: add tests for session creation

--- tests/db.rs
+++ tests/db.rs
@@ -1,5 +1,7 @@
 use cloud::database::Database;
+use model::MacAddress;
 use tokio_postgres::NoTls;
+use uuid::Uuid;
 
 #[tokio::test]
 async fn database_tests() -> anyhow::Result<()> {
@@ -7,6 +9,10 @@ async fn database_tests() -> anyhow::Result<()> {
     let (client, conn) = tokio_postgres::connect(&url, NoTls).await?;
     let db = Database::from(client);
     let handle = tokio::spawn(conn);
+
+    let id = db.create_session(MacAddress([0x55, 0x44, 0x33, 0x22, 0x11, 0x00])).await;
+    assert!(db.is_valid_session(id).await);
+    assert!(!db.is_valid_session(Uuid::nil()).await);
 
     drop(db);
     handle.await??;
