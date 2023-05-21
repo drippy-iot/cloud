7bba94c921bd1ee6f9ae77853b3675ab159e6df7
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-21 17:19:26 +0800

    docs(db): add documentation for database wrappers

--- src/database.rs
+++ src/database.rs
@@ -15,6 +15,9 @@ impl From<Client> for Database {
 }
 
 impl Database {
+    /// Whenever the ESP32 board starts up, it must ping the database about its registration.
+    /// If the MAC address had not been previously registered, we insert it into the database.
+    /// Otherwise, we do nothing. In both cases, we return the latest shutdown state of the unit.
     pub async fn register_unit(&self, mac: MacAddress) -> bool {
         let row = self
             .db
@@ -27,6 +30,8 @@ impl Database {
         row.get(0)
     }
 
+    /// Reports to the database the current water flow of the device.
+    /// Returns the latest shutdown state of the unit.
     pub async fn report_flow(&self, Flow { addr, flow }: Flow) -> bool {
         let flow = i16::try_from(flow).unwrap();
         let row = self
@@ -42,6 +47,8 @@ impl Database {
         row.get(0)
     }
 
+    /// Reports to the database a single instance of a leak detection.
+    /// Returns the latest shutdown state of the unit.
     pub async fn report_leak(&self, mac: MacAddress) -> bool {
         let row = self
             .db
@@ -56,6 +63,9 @@ impl Database {
         row.get(0)
     }
 
+    /// Creates a new session from the given address. If the MAC address had not been previously
+    /// registered (i.e., the ESP32 failed to ping the server before user logged in), this returns
+    /// [`None`]. Otherwise, it returns the [`Uuid`] of the newly created session.
     pub async fn create_session(&self, mac: MacAddress) -> Option<Uuid> {
         let err = match self.db.query_one("INSERT INTO session (mac) VALUES ($1) RETURNING id", &[&mac]).await {
             Ok(row) => return row.try_get(0).ok(),
