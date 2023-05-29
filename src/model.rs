b36c22903ec73d2f8d90a66c8efffcf6cbf10360
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-29 12:28:53 +0800

    feat(model): update schemas to allow multiplexed SSE stream

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,43 @@
 use chrono::{serde::ts_milliseconds, DateTime, Utc};
-use serde::{Deserialize, Serialize};
+use model::MacAddress;
+use serde::{Serialize, Serializer};
 
-#[derive(Serialize, Deserialize)]
+fn mac_to_int<S: Serializer>(mac: &Option<MacAddress>, serializer: S) -> Result<S::Ok, S::Error> {
+    if let Some(MacAddress([a, b, c, d, e, f])) = *mac {
+        let value = u64::from_be_bytes([0, 0, a, b, c, d, e, f]);
+        serializer.serialize_some(&value)
+    } else {
+        serializer.serialize_none()
+    }
+}
+
+#[derive(Serialize)]
+pub struct Header {
+    /// MAC address to which the metrics is associated with.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(serialize_with = "mac_to_int")]
+    pub mac: Option<MacAddress>,
+    /// Exact time of the snapshot.
+    pub timestamp: DateTime<Utc>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "ty")]
+pub enum Payload {
+    Flow { flow: u16 },
+    Leak,
+    Shutdown,
+    Reset,
+}
+
+#[derive(Serialize)]
+pub struct Message {
+    pub head: Header,
+    pub data: Payload,
+}
+
+#[derive(Serialize)]
 pub struct ClientFlow {
     // Creation data of the data point
     #[serde(with = "ts_milliseconds")]
